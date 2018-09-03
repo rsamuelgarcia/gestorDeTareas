4d82bca7cb64dd8e52f1704077ea607c50090a01
= <=>
2018-09-03 09:59:08 -0400

    cambios minimos

--- app.js
+++ app.js
@@ -3,17 +3,70 @@ const formTareas = document.querySelector('#formTarea');
 formTareas.addEventListener("submit", guardarTarea);
 const listaTareas = document.querySelector('#lista-tareas');
 listaTareas.addEventListener('click', eliminarTarea);
+window.addEventListener('load', mostrarTareasDeLocalStorage);
 function guardarTarea(e) {
     e.preventDefault();
-    let tareas = document.createElement('li');
+    let objetoTarea = creacionDeNodo();
+    creacionDeBotonEliminar(objetoTarea.tarea);
+    let lista = document.getElementById('lista-tareas');
+    lista.appendChild(objetoTarea.tarea);
+    guardarEnLocalStorage(objetoTarea.texto);
+}
+function creacionDeNodo() {
+    let tarea = document.createElement('li');
     let textoForm = document.querySelector('#tarea');
     let textoTarea = document.createTextNode(textoForm.value);
-    tareas.appendChild(textoTarea);
-    let lista = document.getElementById('lista-tareas');
-    lista.appendChild(tareas);
+    tarea.appendChild(textoTarea);
+    textoForm.value = "";
+    let objetoTarea = {
+        tarea: tarea,
+        texto: tarea.innerText
+    };
+    return objetoTarea;
+}
+function creacionDeBotonEliminar(tarea) {
+    let eliminar = document.createElement('a');
+    eliminar.setAttribute('href', '#');
+    eliminar.appendChild(document.createTextNode('X'));
+    tarea.appendChild(eliminar);
 }
 function eliminarTarea(e) {
     e.preventDefault();
-    console.log(e.target.nodeType());
+    if (e.target.tagName == 'A') {
+        eliminarDeLocalStorage(e.target.parentElement);
+        e.target.parentElement.remove();
+    }
+}
+function guardarEnLocalStorage(tarea) {
+    let tareas = verificarExistenciaDeArray();
+    tareas.push(tarea);
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+}
+function verificarExistenciaDeArray() {
+    let tareas;
+    if (localStorage.getItem('tareas') == null) {
+        tareas = [];
+    }
+    else {
+        tareas = JSON.parse(localStorage.getItem('tareas'));
+    }
+    return tareas;
+}
+function mostrarTareasDeLocalStorage() {
+    let tareas = verificarExistenciaDeArray();
+    tareas.forEach(function (tarea) {
+        let tareaNodo = document.createElement('li');
+        let textoTarea = document.createTextNode(tarea);
+        tareaNodo.appendChild(textoTarea);
+        creacionDeBotonEliminar(tareaNodo);
+        let lista = document.getElementById('lista-tareas');
+        lista.appendChild(tareaNodo);
+    });
+}
+function eliminarDeLocalStorage(elemento) {
+    let palabra = elemento.innerText.substring(0, elemento.innerText.length - 1);
+    let tareas = verificarExistenciaDeArray();
+    tareas.splice(tareas.indexOf(palabra), 1);
+    localStorage.setItem('tareas', JSON.stringify(tareas));
 }
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,6 +1,4 @@
-
-
- const formTareas = <HTMLFormElement>document.querySelector('#formTarea')
+const formTareas = <HTMLFormElement>document.querySelector('#formTarea')
  formTareas.addEventListener("submit", guardarTarea)
 
  const listaTareas = <HTMLDivElement>document.querySelector('#lista-tareas')
@@ -8,27 +6,117 @@
  listaTareas.addEventListener('click', eliminarTarea)
 
 
+ window.addEventListener('load', mostrarTareasDeLocalStorage)
 
 
-/* ================================= */
 
-function guardarTarea(e:any){
+
+/* ===========================================================*/
+
+function guardarTarea(e:Event){
 
     e.preventDefault()
 
-    let tareas = <HTMLElement> document.createElement('li')
+    let objetoTarea = creacionDeNodo()
+
+    creacionDeBotonEliminar(objetoTarea.tarea)
+
+    let lista = <HTMLDivElement> document.getElementById('lista-tareas')
+    lista.appendChild(objetoTarea.tarea)
+
+    guardarEnLocalStorage(objetoTarea.texto)
+
+}
+
+function creacionDeNodo():any{
+
+    let tarea = <HTMLElement> document.createElement('li')
     let textoForm = <HTMLInputElement> document.querySelector('#tarea')
     let textoTarea = <Text> document.createTextNode(textoForm.value)
+    tarea.appendChild(textoTarea)
 
-    tareas.appendChild(textoTarea)
+    textoForm.value = ""
 
-    let lista  =<HTMLDivElement> document.getElementById('lista-tareas')
-    lista.appendChild(tareas)
+    let objetoTarea = {
+        tarea:tarea,
+        texto:tarea.innerText
+    }
+    return objetoTarea
 }
+
+function creacionDeBotonEliminar(tarea:HTMLElement){
+
+    let eliminar = <HTMLElement> document.createElement('a')
+    eliminar.setAttribute('href', '#')
+    eliminar.appendChild(document.createTextNode('X'))
+    tarea.appendChild(eliminar)
+}
+
 
 function eliminarTarea(e:any){
     e.preventDefault()
 
-    console.log(e.target.nodeType())
-    
+    if(e.target.tagName == 'A'){
+        
+        eliminarDeLocalStorage(e.target.parentElement);
+        <HTMLElement> e.target.parentElement.remove()
+        
+        
+    }
+
+}
+
+function guardarEnLocalStorage (tarea:string):void {
+
+    let tareas:string[] = verificarExistenciaDeArray()
+
+    tareas.push(tarea)
+  
+    localStorage.setItem('tareas', JSON.stringify(tareas))
+
+}
+
+function verificarExistenciaDeArray():string[]{
+
+    let tareas:string[]
+
+    if(localStorage.getItem('tareas') == null){
+
+        tareas = []
+
+    }else{
+
+        tareas = JSON.parse(<string>localStorage.getItem('tareas'))
+        
+    }
+    return tareas
+}
+
+function mostrarTareasDeLocalStorage(){
+
+    let tareas = verificarExistenciaDeArray()
+
+    tareas.forEach(function(tarea){
+
+    let tareaNodo = <HTMLElement> document.createElement('li')
+    let textoTarea = <Text> document.createTextNode(tarea)
+    tareaNodo.appendChild(textoTarea)
+
+    creacionDeBotonEliminar(tareaNodo)
+
+    let lista = <HTMLDivElement> document.getElementById('lista-tareas')
+    lista.appendChild(tareaNodo)
+
+    })
+
+}
+
+function eliminarDeLocalStorage(elemento:any){
+    let palabra = elemento.innerText.substring(0, elemento.innerText.length - 1)
+
+    let tareas = verificarExistenciaDeArray()
+
+    tareas.splice(tareas.indexOf(palabra), 1)
+
+    localStorage.setItem('tareas', JSON.stringify(tareas))
 }
